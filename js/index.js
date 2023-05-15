7d8713d0c508e619742456bfa4d44c3f757e54ae
crh-nathansaludes <n.saludes@creativehope.co.jp>
2023-05-15 12:44:05 +0800

    PRG-01: Add javascript for 'Tabs' component.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-// Accordion Script
+/* Accordion Script
+---------------------------------------------------------------------------------------- */
 window.onload = function () {
   $("footer .container-1 .group-1 .tab-content").html(
     $("footer .container-1 .group-1 .accordion .item.active .content").html()
@@ -19,5 +20,25 @@ $("footer .container-1 .group-1 .accordion .item").click(function () {
 
     $("footer .container-1 .group-1 .accordion .item.active").removeClass("active");
     $(this).toggleClass("active");
+  }
+});
+
+
+/* Tabs Script
+---------------------------------------------------------------------------------------- */
+$(".tabs .tab-item").on("click", function () {
+  if ($(this).hasClass("active") === false) {
+    const tab_index = $(this).attr('data-tab-index');
+
+    // remove active class from current active item
+    $(".tabs .tab-item.active").removeClass("active");
+    $(".tabs .content-area .inner-container.active").removeClass("active");
+
+    // set new active item
+    $(this).toggleClass("active");
+    $(`.tabs .content-area .inner-container[data-content-index="${tab_index}"]`).toggleClass("active");
+  }
+  else {
+    // ignore
   }
 });
